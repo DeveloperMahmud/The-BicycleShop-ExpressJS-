f9ef3e7888b17bf1029a77f301c5a333b714dcbf
prince <mrepte700@gmail.com>
2021-06-28 12:00:16 +0600

    handle static files + install Npm ejs

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bicycles = require('./data/data.json');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/',(req,res) =>{
+   return res.render('bicycles');
+});
+app.get('/bicycle',(req,res) =>{
+    console.log(req.query.id); 
+    const bicycle = bicycles.find(b => b.id ===req.query.id);
+    return res.render('overview');
+});
+
+app.listen(3000, ()=> {
+    console.log('server is running at port: 3000');
+});
