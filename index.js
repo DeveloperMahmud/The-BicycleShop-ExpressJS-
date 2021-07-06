77c79cdf53ad73e0cfd2446d061aa7c19d75298d
prince <mrepte700@gmail.com>
2021-07-07 01:33:25 +0600

    Dynamic Port Binding

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.get('/bicycle',(req,res) =>{
     });
 });
 
-app.listen(3000, ()=> {
-    console.log('server is running at port: 3000');
+let port = process.env.PORT || 3000
+app.listen(port, ()=> {
+    console.log(`server is running at port: ${port}`);
 });
